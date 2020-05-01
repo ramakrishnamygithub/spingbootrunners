af93e0aac4d16bef4f5f3036f428e9f6a4019a8b
ramakrishna <ramakrishna@LAPTOP-QSONDSEP>
2020-05-01 10:12:35 +0530

    First Commit

---
+++ src/main/java/com/example/demo/MyBatchRunner.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+@Component
+@Order(0)
+public class MyBatchRunner implements CommandLineRunner,Ordered{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+System.out.println("MyBatchRunner...");
+	}
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+}
---
+++ src/main/java/com/example/demo/MyEmailRunner.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+@Component
+@Order(1)
+public class MyEmailRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("MyEmailRunner....");
+
+	}
+
+}
